9c7bdbe233153dac4b5d05940466090d4731cf22
Sudhan-J <sudhanj27@gmail.com>
2021-06-15 16:13:45 +0530

    Create ROT13.c

---
+++ ROT13.c
@@ -0,0 +1,112 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<ctype.h>
+char ch,cur;
+char input_character[200],op[200];
+void encrypt()
+{
+    int i,j=0;
+    for(i=0;input_character[i]!='\0';i++,j++)
+    {
+        ch=input_character[i];
+        ch = tolower(ch);
+        switch(ch)
+        {
+        case 'a':
+            op[j] = 'n';
+            break;
+        case 'b':
+            op[j] = 'o';
+            break;
+        case 'c':
+            op[j]='p';
+            break;
+        case 'd':
+            op[j] = 'q';
+            break;
+        case 'e':
+            op[j] = 'r';
+            break;
+        case 'f':
+            op[j] = 's';
+            break;
+        case 'g':
+            op[j] = 't';
+            break;
+        case 'h':
+            op[j] = 'u';
+            break;
+        case 'i':
+            op[j] = 'v';
+            break;
+        case 'j':
+            op[j] = 'w';
+            break;
+        case 'k':
+            op[j] = 'x';
+            break;
+        case 'l':
+            op[j] = 'y';
+            break;
+        case 'm':
+            op[j] = 'z';
+            break;
+        case 'n':
+            op[j] = 'a';
+            break;
+        case 'o':
+            op[j] = 'b';
+            break;
+        case 'p':
+            op[j] = 'c';
+            break;
+        case 'q':
+            op[j] = 'd';
+            break;
+        case 'r':
+            op[j] = 'e';
+            break;
+        case 's':
+            op[j] = 'f';
+            break;
+        case 't':
+            op[j] = 'g';
+            break;
+        case 'u':
+            op[j] = 'h';
+            break;
+        case 'v':
+            op[j] = 'i';
+            break;
+        case 'w':
+            op[j] = 'j';
+            break;
+        case 'x':
+            op[j] = 'k';
+            break;
+        case 'y':
+            op[j] = 'l';
+            break;
+        case 'z':
+            op[j] = 'm';
+            break;
+        case ' ':
+            op[j] = ' ';
+            break;
+        default:
+            if(!isalnum(ch))
+            {
+                op[j] = ch;
+            }
+        }
+        fflush(stdin);
+    }
+}
+void main()
+{
+    printf("Enter the string:\n");
+    gets(input_character);
+    encrypt();
+    puts(op);
+}
